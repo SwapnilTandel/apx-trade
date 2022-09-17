06fd349dafb075d8e12442ef6498c0f4ede0b74d
Swapnil Tandel <swapniltandel@JangMen.local>
2022-09-17 14:58:25 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Mosaic, MosaicWindow } from "react-mosaic-component";
+
+import MetaWiki from "./components/MetaRadio";
+import Calendar from "./components/Calendar";
+// import MetaRadio from "./components/MetaRadio";
+import Radio from "./components/Radio";
+import SeedPool from "./components/SeedPool";
+
+import "react-mosaic-component/react-mosaic-component.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+import "./styles.css";
+
+export type ViewId = "a" | "b" | "c" | "new";
+
+const config = {
+};
+
+// const APP_MAP: Record<ViewId, string> = {
+//   a: <MetaWiki />,
+//   b: <SeedPool />,
+//   c: <MetaRadio />,
+//   new: "New Window"
+// };
+
+// export const mosaicWindow = (
+//   <Mosaic<ViewId>
+//     className="mosaic-blueprint-theme bp3-dark"
+//     renderTile={(id, path) => (
+//       <MosaicWindow<ViewId>
+//         config={config}
+//         path={path}
+//         createNode={() => <Calendar />}
+//         title={id}
+//       >
+//         <div style={{ width: "100%", height: "100%" }}>{APP_MAP[id]}</div>
+//       </MosaicWindow>
+//     )}
+//     initialValue={{
+//       direction: "row",
+//       first: "a",
+//       second: {
+//         direction: "column",
+//         first: "b",
+//         second: "c"
+//       }
+//     }}
+//   />
+// );
+
+const ELEMENT_MAP: { [viewId: string]: JSX.Element } = {
+  a: <Radio />,
+  b: <div>Top Right Window</div>,
+  c: <div>Bottom Right Window</div>,
+};
+
+export const mosaicWindow = (
+  <div id="app">
+    <Mosaic<string>
+      renderTile={(id) => ELEMENT_MAP[id]}
+      initialValue={{
+        direction: 'row',
+        first: 'a',
+        second: {
+          direction: 'column',
+          first: 'b',
+          second: 'c',
+        },
+        splitPercentage: 40,
+      }}
+    />
+  </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return mosaicWindow;
 }
 
 export default App;
---
+++ src/components/Radio.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Radio = () => {
+    return (
+        <iframe
+        title="MetaRadio"
+        src="https://anchor.fm/MetaGame/"
+        style={{
+          height: `100%`,
+          width: `100%`
+        }}
+      />
+    )
+}
+
+export default Radio
